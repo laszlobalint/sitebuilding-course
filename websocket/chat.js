265d9ff4055f52e2846d9a34ad65465a7a5cacaf
Balint Laszlo <balint.laszlo@t-systems.com>
2020-07-07 16:04:52 +0200

    first room

--- websocket/chat.js
+++ websocket/chat.js
@@ -9,7 +9,14 @@ const io = socketio(expressServer);
 
 io.on('connection', (socket) => {
   socket.emit('messageFromServer', { data: 'Welcome to the Socket.IO Server!' });
-  socket.on('newMessageToServer', (newMessage) => {
-    io.emit('messageToClients', { text: newMessage.text });
+  socket.on('messageToServer', (newMessage) => {
+    console.log(newMessage);
   });
+  socket.join('tempRoom');
+  io.to('tempRoom').emit('joined', `${socket.id} says: You have joined the TempRoom!`);
+});
+
+io.of('/admin').on('connection', (socket) => {
+  console.log('Logged to admin namespace...');
+  io.of('/admin').emit('welcome', 'Welcome to the admin channel!');
 });
