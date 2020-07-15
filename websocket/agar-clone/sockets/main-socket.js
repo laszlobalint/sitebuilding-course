015f277257a82e932b20f487250560377919c6ab
Balint Laszlo <balint.laszlo@t-systems.com>
2020-07-15 11:25:06 +0200

    Set up player object

--- websocket/agar-clone/sockets/main-socket.js
+++ websocket/agar-clone/sockets/main-socket.js
@@ -1,19 +1,33 @@
 const io = require('../servers').io;
+const PlayerConfiguration = require('./classes/PlayerConfiguration');
+const PlayerPublicData = require('./classes/PlayerPublicData');
+const Player = require('./classes/Player');
 const Orb = require('./classes/Orb');
 
 let orbs = [];
+let settings = {
+  defaultOrbs: 500,
+  defaultSpeed: 6,
+  defaultSize: 6,
+  defaultZoom: 1.5,
+  worldWidth: 500,
+  worldHeight: 500,
+};
 
 initGame();
 
 io.sockets.on('connection', (socket) => {
+  let playerConfig = new PlayerConfiguration(settings);
+  let playerData = new PlayerPublicData(null, settings);
+  let player = new Player(socket.id, playerConfig, playerData);
   socket.emit('init', {
     orbs,
   });
 });
 
 function initGame() {
-  for (let i = 0; i < 500; i++) {
-    orbs.push(new Orb());
+  for (let i = 0; i < settings.defaultOrbs; i++) {
+    orbs.push(new Orb(settings));
   }
 }
 
